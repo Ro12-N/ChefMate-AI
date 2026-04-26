e0b3f9d43ec5c9a0d8c3f04a51b298f10556f921
ro12-n <rohannaidu940@gmail.com>
2026-04-26 15:17:01 +0530

    Dashboard Pantry Recipes

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 import Header from "@/components/Header";
 import { neobrutalism } from "@clerk/themes";
 import Image from "next/image";
@@ -8,8 +9,8 @@ import Image from "next/image";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "ChefMate AI - Your Personal Recipe Assistant",
-  description: "Discover, create, and manage your recipes with ChefMate AI. Your personal kitchen companion powered by AI.",
+  title: "Servd - AI Recipes Platform",
+  description: "",
 };
 
 export default function RootLayout({ children }) {
@@ -21,26 +22,27 @@ export default function RootLayout({ children }) {
     >
       <html lang="en" suppressHydrationWarning>
         <head>
-          <link rel="icon" href="/blacklogo.png" sizes="any" />
+          <link rel="icon" href="/logo.png" sizes="any" />
         </head>
         <body className={`${inter.className}`}>
           <Header />
           <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
 
           {/* Footer */}
           <footer className="py-8 px-4 border-t">
             <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center gap-6">
               <div className="flex items-center gap-3">
                 <Image
-                  src="/blacklogo.png"
-                  alt="ChefMate Logo"
+                  src="/logo.png"
+                  alt="Servd Logo"
                   width={48}
                   height={48}
                   className="w-14"
                 />
               </div>
               <p className="text-stone-500 text-sm">
-                Made with 💗 by RohanNaidu
+                Made with 💗 by RoadsideCoder
               </p>
             </div>
           </footer>
